03c67527db120c68a39621a569f9b1efc10accd1
cdzwm <495720919@qq.com>
2021-09-28 15:59:17 +0800

    Add files via upload

--- checkip.pl
+++ checkip.pl
@@ -8,21 +8,29 @@ use Try::Tiny;
 
 my @check_ports = qw(80 443);
 
-my @hosts = qw(202.99.166.4);
+my %hosts;
+
+for( qw(www.baidu.com 202.99.166.4 sina.com.cn) ) {
+	$hosts{$_} = 0
+}
 
 my $ping = Net::Ping->new('tcp', 2);
 
 while(1){
-	for (@hosts){
-		for my $port (@check_ports) {
-			$ping->port_number($port);
-			if( my $result = $ping->ping($_)){
-				&notify(scalar localtime . ' : ' .  "Host $_, Port: $port");
-				last;
+	for (keys %hosts){
+		unless ( $hosts{$_} ){
+			for my $port (@check_ports) {
+				$ping->port_number($port);
+				if( my $result = $ping->ping($_)){
+					$hosts{$_} = 1;
+					&notify(scalar localtime . ' : ' .  "Host $_, Port: $port");
+					say $_;
+					last;
+				}
 			}
 		}
 	}
-	sleep 60;
+	sleep 60*10;
 }
 
 sub notify{
@@ -31,15 +39,15 @@ sub notify{
 		host => 'smtp.qq.com',
 		ssl  => 'ssl',
 		port => 465,
-		sasl_username => '2084944805@qq.com',
-		sasl_password => 'password',
+		sasl_username => '111111111@qq.com',
+		sasl_password => 'xxxxxxxxxxxxxxxx',
 		debug => 0
 	);
 
 	my $message = Email::Simple->create(
 		header => [
-			From    => '11111111@qq.com',
-			To      => '22222222@qq.com',
+			From    => '11111111111@qq.com',
+			To      => '11111111111@qq.com',
 			Subject => 'Please note the host is online',
 		],
 		body => $text,
